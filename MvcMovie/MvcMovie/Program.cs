424a75f6228ae525738f5492cc74dcdfdeafafe2
Austin <g42pixels@gmail.com>
2022-02-07 12:12:33 +0200

    DB seed
    Initial Seed

--- MvcMovie/MvcMovie/Program.cs
+++ MvcMovie/MvcMovie/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -16,11 +17,13 @@ namespace MvcMovie
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
